7dfbb037fa8444a1da21e3cf4c666e6474acfb64
Cameron <hackwithcameron@gmail.com>
2020-04-14 13:57:49 -0700

    Yesterday Card
    Added card with stats from the previous day. Altered CovidScraper to gather yesterdays and todays Covid information.

--- COVIDPage/scripts/COVIDCrawler/covidCrawler.py
+++ COVIDPage/scripts/COVIDCrawler/covidCrawler.py
@@ -1,41 +1,40 @@
 import requests
 from bs4 import BeautifulSoup
 
-searchedState = 'oregon'
 
+class CovidScraper:
+    def __init__(self, tableTag):
+        # URL to be scraped
+        URL = 'https://www.worldometers.info/coronavirus/country/us/'
+        statePage = requests.get(URL)
+        soup = BeautifulSoup(statePage.content, 'html.parser')
+        # Specifies table on page
+        self.table = soup.find(id=tableTag)
 
-# URL to be scraped
-URL = 'https://www.worldometers.info/coronavirus/country/us/#nav-today'
-statePage = requests.get(URL)
-soup = BeautifulSoup(statePage.content, 'html.parser')
-# Specifies table on page
-table = soup.find(id='usa_table_countries_today')
+    # Gets header for list
+    def headerScraper(self):
+        headers = self.table.find_all('th')
 
-# Gets header for list
-def headerScraper():
-    headers = table.find_all('th')
+        headerInfo = []
+        # Searches each cell to match state searched
+        for header in headers:
+            headerInfo.append(header.get_text(separator=" "))
+        return headerInfo
 
-    headerInfo = []
-    # Searches each cell to match state searched
-    for header in headers:
-        headerInfo.append(header.get_text(separator=" "))
-    return headerInfo
+    # Gets searched states information
+    def stateScraper(self, searchedState):
+        cells = self.table.find_all('td')
 
-
-# Gets searched states information
-def stateScraper(searchedState):
-    cells = table.find_all('td')
-
-    stateInfo = []    # Empty list to hold parsed information
-    for cell in cells:    # Searches each cell to match state searched
-        if cell.text.strip().lower() == searchedState.lower():
-            info = cell.find_next_siblings('td')      # Gets all 'td' cells up to the next 'tr'
-            stateInfo.append(cell.text.strip())     # Append searched name to list
-            for i in info:      # Gets information from each cell following the searched name
-                if i.text.strip() == '':     # Replaces blank cells with a '0'
-                    stateInfo.append('0')
-                else:
-                    stateInfo.append(i.text.strip())        # Appends information from cells following searched name
-    return stateInfo
+        stateInfo = []    # Empty list to hold parsed information
+        for cell in cells:    # Searches each cell to match state searched
+            if cell.text.strip().lower() == searchedState.lower():
+                info = cell.find_next_siblings('td')      # Gets all 'td' cells up to the next 'tr'
+                stateInfo.append(cell.text.strip())     # Append searched name to list
+                for i in info:      # Gets information from each cell following the searched name
+                    if i.text.strip() == '':     # Replaces blank cells with a '0'
+                        stateInfo.append('0')
+                    else:
+                        stateInfo.append(i.text.strip())        # Appends information from cells following searched name
+        return stateInfo
 
 
--- COVIDPage/views.py
+++ COVIDPage/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .scripts.COVIDCrawler.covidCrawler import *
+from .scripts.COVIDCrawler.covidCrawler import CovidScraper
 
 # Toggle for running scraper
 run = True
@@ -7,11 +7,13 @@ run = True
 
 def index(request):
     if run:
-        headerInfo = headerScraper()
-        stateInfo = stateScraper('Oregon')
+        headerInfo = CovidScraper.headerScraper(CovidScraper(tableTag='nav-today'))
+        stateInfo = CovidScraper.stateScraper(CovidScraper(tableTag='nav-today'), searchedState='Oregon')
+        pastInfo = CovidScraper.stateScraper(CovidScraper(tableTag='nav-yesterday'), searchedState='Oregon')
         context = {
             "headerInfo": headerInfo,
-            "stateInfo": stateInfo
+            "stateInfo": stateInfo,
+            "pastInfo": pastInfo
         }
         return render(request, 'COVIDPage/index.html', context)
     else:
