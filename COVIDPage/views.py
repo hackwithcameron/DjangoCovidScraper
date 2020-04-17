effe03450a3c7fe1b41930d9301ed91d7c55772e
Cameron <hackwithcameron@gmail.com>
2020-04-17 12:56:28 -0700

    Check search
    Added check with JSON state list if searched state does not exsist in list, display default.

--- COVIDPage/views.py
+++ COVIDPage/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render
 from .scripts.COVIDCrawler.covidCrawler import CovidScraper
-
-# Toggle for running scraper
-run = True
+import json
 
 
 def index(request):
     searchedState = request.POST.get('searchedState')
-    if not (searchedState == ''):
+    # Opens JSON file to check for states
+    with open('./static/json/states.json', 'r') as stateList:
+        x = stateList.read()
+    # check to see if search bar includes a state
+    if not (searchedState == '') and (searchedState.lower() in x.lower()):
         if request.method == 'POST':
-            searchedState = request.POST.get('searchedState')
             headerInfo = CovidScraper.headerScraper(CovidScraper(tableTag='nav-today'))
             stateInfo = CovidScraper.stateScraper(CovidScraper(tableTag='nav-today'), searchedState=searchedState)
             pastInfo = CovidScraper.stateScraper(CovidScraper(tableTag='nav-yesterday'), searchedState=searchedState)
