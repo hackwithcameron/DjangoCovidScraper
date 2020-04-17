9cae7dd73f751e538925627c0328bd74a319725b
Cameron <hackwithcameron@gmail.com>
2020-04-16 19:00:45 -0700

    Mobile friendly
    Made UI styling changes at mobile break point.

--- COVIDPage/models.py
+++ COVIDPage/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+import datetime
 
-# Create your models here.
+
+class CovidData(models.Model):
+    date = models.DateField(primary_key=True)
+    state = models.CharField(max_length=50)
+    total_cases = models.IntegerField()
+    new_case = models.IntegerField()
+    total_deaths = models.IntegerField()
+    new_deaths = models.IntegerField()
+    active_case = models.IntegerField()
